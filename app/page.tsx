7b5c386d6a872b6ab5581b9bf1a32f4343b259ab
Wesdell <wesdell17@gmail.com>
2023-06-20 21:04:22 -0500

    Home page created

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 export default function Home () {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Find & Share
+        <br/>
+        <span className="orange_gradient text-center">
+          AI Powered Prompts
+        </span>
+      </h1>
+      <p className="desc text-center">
+        Prompthat is an open-source AI promps tool for
+        modern world to find, create and share creative prompts
+      </p>
+    </section>
   );
 }
