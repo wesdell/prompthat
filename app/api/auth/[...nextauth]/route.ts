2c6a5e3004cbd66048e613bfca6b0c8482b38b05
Wesdell <wesdell17@gmail.com>
2023-06-27 21:28:25 -0500

    Endpoint for manage Google auth with next-auth

---
+++ app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,25 @@
+import NextAuth, { type Session } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+import { dbUser } from '@/database';
+
+const handler = NextAuth ({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ''
+    })
+  ],
+  callbacks: {
+    async session ({ session }): Promise<Session> {
+      const sessionUser = await dbUser.findUser({ email: session.user?.email });
+      session.user.id = sessionUser?.toString();
+      return session;
+    },
+    async signIn ({ profile }) {
+      return await dbUser.createUser({ username: profile?.name?.replace(' ', '').toLowerCase(), email: profile?.email, image: profile?.image });
+    }
+  }
+});
+
+export { handler as GET, handler as POST };
