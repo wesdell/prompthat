281b5e8ab248aa2cc7e86023d53f0f72a5a421ec
Wesdell <wesdell17@gmail.com>
2023-07-04 21:41:17 -0500

    Endpoint to get user prompts

---
+++ app/api/users/[id]/prompts/route.ts
@@ -0,0 +1,21 @@
+import { NextResponse } from 'next/server';
+
+import { db } from '@/database';
+import { Prompt } from '@/models';
+
+interface Params {
+  params: {
+    id: string
+  }
+}
+
+export async function GET (request: Request, { params }: Params) {
+  try {
+    await db.connectToDB();
+    const prompts = await Prompt.find({ creator: params.id }).populate('creator');
+    return NextResponse.json(prompts);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json([]);
+  }
+}
